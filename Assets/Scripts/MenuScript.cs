7ca691bf8cc080ec4bc56ea51342b33c0e4d444e
JeffrinDumas <jeffrin.e.d@live.nl>
2017-11-22 15:52:22 +0100

    New concept and finished game

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -10,35 +10,21 @@ public class MenuScript : MonoBehaviour {
     public GameObject levelButtonContainer;
     public GameObject shopButton;
     public GameObject shopButtonContainer;
+    public GameObject imageField;
+
+    [SerializeField]
+    private GameObject _startBtn;
+    
 
     private Transform _cameraTransform;
     private Transform _cameraPrefferedLookat;
+    
 
     private const float _rotationSpeed = 2.5f;
+
     private void Start()
     {
         _cameraTransform = Camera.main.transform;
-        for (int i = 0; i < 3; i++)
-        {
-            GameObject container = Instantiate(levelButton) as GameObject;
-            
-            container.transform.SetParent(levelButtonContainer.transform,false);
-          
-            string levelName = levelButton.name + i.ToString();
-            container.GetComponent<Button>().onClick.AddListener(() => _LoadLevel(levelName)); 
-        }
-
-        for (int i = 0; i < 3; i++)
-        {
-            GameObject container = Instantiate(shopButton) as GameObject;
-
-            container.transform.SetParent(shopButtonContainer.transform, false);
-
-            string shopName = shopButton.name + i.ToString();
-            container.GetComponent<Button>().onClick.AddListener(() => _shopItem(shopName));
-        }
-
-
     }
 
     private void Update()
@@ -49,18 +35,14 @@ public class MenuScript : MonoBehaviour {
         }
     }
 
-    private void _LoadLevel(string levelName)
-    {
-        SceneManager.LoadScene(levelName);
-    }
 
-    private void _shopItem(string shopName)
-    {
-        SceneManager.LoadScene(shopName);
-    }
 
-    public void LookAtMenu(Transform menuTransform)
+        public void LookAtMenu(Transform menuTransform)
     {
         _cameraPrefferedLookat = menuTransform;
     }
+
+   
+
+
 }
